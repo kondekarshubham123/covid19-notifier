e5e89d9a85695121190ea9c46118a08849290264
Shubham Kondekar <kondekarshubham123@gmail.com>
2021-05-06 21:24:04 +0530

    Credential Template file added

---
+++ credential_temp.py
@@ -0,0 +1,25 @@
+class Sender(object):
+    def __init__(self):
+        self.__senderEmail = 'sender@gmail.com' # Change here
+        self.__senderpwd   = 'XXXXXX'           # Type password here
+    
+    def get_email(self):
+        return self.__senderEmail
+    
+    def get_senderpwd(self):
+        return self.__senderpwd 
+
+class Receiver(object):
+    def __init__(self,fname,lname,email,pin,date):
+        self.fname = fname
+        self.lname = lname
+        self.email = email
+        self.pincd = pin
+        self.date  = date
+
+
+SendMain = Sender()
+
+rcv1 = Receiver('Firstname','Lastname','receiveremail@gmail.com','413512','06-05-2021')
+
+Users = [rcv1]
