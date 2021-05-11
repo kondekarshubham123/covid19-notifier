830a2be9a6edd7723bebcd0dcea55d64e1146cbf
Shubham Kondekar <kondekarshubham123@gmail.com>
2021-05-11 12:26:26 +0530

    [Functionality Add] Notify by music

--- code.py
+++ code.py
@@ -3,23 +3,26 @@ import time
 import json
 import requests
 import threading
-from notifier import emailsend,dummySend
+from notifier import emailsend, dummySend, songNotify
 from credential import Users
-
 
 
 def User_check(user):
     while True:
-        time.sleep(10)
-        api= "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode="+user.pincd+"&date="+user.date
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-        response = requests.get(api, headers = headers)
+        time.sleep(3)
+        api = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=" + \
+            user.pincd+"&date="+user.date
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        response = requests.get(api, headers=headers)
         op = json.loads(response.text)
         if op['sessions'] == []:
             pass
         else:
-            dummySend(user,op)
+            songNotify(user, op)
+            dummySend(user, op)
             sys.exit()
+
 
 for use in Users:
     # User_check(use)
--- notifier.py
+++ notifier.py
@@ -6,8 +6,8 @@ from email.mime.multipart import MIMEMultipart
 import datetime
 
 
-def emailsend(user,data):
-    
+def emailsend(user, data):
+
     mail_content = '''Hello,
     Below are the list of covid centers.\n
     
@@ -19,45 +19,47 @@ def emailsend(user,data):
     for hos in data['sessions']:
         # print(hos['name'],hos['address'],hos['state_name'],hos['pincode'],hos['fee_type'],hos['date'],hos['available_capacity'],
         # hos['fee'],hos['min_age_limit'],hos['vaccine'])
-        
-        lis += "Hospital name       :   " +hos['name']                    + '\n'
-        lis += "Hospital Pincode    :   " +str(hos['pincode'])            + '\n'
-        lis += "Fee type            :   " +str(hos['fee_type'])           + '\n'
-        lis += "Available Capacity  :   " +str(hos['available_capacity']) + '\n'
-        lis += "Age Limit           :   " +str(hos['min_age_limit'])      + '\n'
-        lis += "Vaccine type        :   " +hos['vaccine']                 + '\n'
+
+        lis += "Hospital name       :   " + hos['name'] + '\n'
+        lis += "Hospital Pincode    :   " + str(hos['pincode']) + '\n'
+        lis += "Fee type            :   " + str(hos['fee_type']) + '\n'
+        lis += "Available Capacity  :   " + \
+            str(hos['available_capacity']) + '\n'
+        lis += "Age Limit           :   " + str(hos['min_age_limit']) + '\n'
+        lis += "Vaccine type        :   " + hos['vaccine'] + '\n'
         lis += '----------------------\n'
-    
+
     mail_content += lis
 
-    #The mail addresses and password
+    # The mail addresses and password
     sender_address = SendMain.get_email()
     sender_pass = SendMain.get_senderpwd()
 
     receiver_address = user.email
 
-    #Setup the MIME
+    # Setup the MIME
     message = MIMEMultipart()
     message['From'] = sender_address
     message['To'] = receiver_address
-    message['Subject'] = 'Vaccine Update !!'   #The subject line
-    
-    #The body and the attachments for the mail
+    message['Subject'] = 'Vaccine Update !!'  # The subject line
+
+    # The body and the attachments for the mail
     message.attach(MIMEText(mail_content, 'plain'))
 
-    #Create SMTP session for sending the mail
-    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
-    session.starttls() #enable security
-    session.login(sender_address, sender_pass) #login with mail_id and password
+    # Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587)  # use gmail with port
+    session.starttls()  # enable security
+    # login with mail_id and password
+    session.login(sender_address, sender_pass)
     text = message.as_string()
     session.sendmail(sender_address, receiver_address, text)
     session.quit()
 
-    print('Mail Sent',user.email)
+    print('Mail Sent', user.email)
 
 
-def dummySend(user,data):
-    
+def dummySend(user, data):
+
     mail_content = '''Hello,
     Below are the list of covid centers.\n
     
@@ -69,14 +71,18 @@ def dummySend(user,data):
     for hos in data['sessions']:
         # print(hos['name'],hos['address'],hos['state_name'],hos['pincode'],hos['fee_type'],hos['date'],hos['available_capacity'],
         # hos['fee'],hos['min_age_limit'],hos['vaccine'])
-        
-        lis += "Hospital name       :   " +hos['name']                    + '\n'
-        lis += "Hospital Pincode    :   " +str(hos['pincode'])            + '\n'
-        lis += "Fee type            :   " +str(hos['fee_type'])           + '\n'
-        lis += "Available Capacity  :   " +str(hos['available_capacity']) + '\n'
-        lis += "Age Limit           :   " +str(hos['min_age_limit'])      + '\n'
-        lis += "Vaccine type        :   " +hos['vaccine']                 + '\n'
+
+        lis += "Hospital name       :   " + hos['name'] + '\n'
+        lis += "Hospital Pincode    :   " + str(hos['pincode']) + '\n'
+        lis += "Fee type            :   " + str(hos['fee_type']) + '\n'
+        lis += "Available Capacity  :   " + \
+            str(hos['available_capacity']) + '\n'
+        lis += "Age Limit           :   " + str(hos['min_age_limit']) + '\n'
+        lis += "Vaccine type        :   " + hos['vaccine'] + '\n'
         lis += '----------------------\n'
 
-    print(mail_content+lis,datetime.datetime.now())
+    print(mail_content+lis, datetime.datetime.now())
 
+
+def songNotify(user, data):
+    os.system('mpg123 src/iphone.mp3')
